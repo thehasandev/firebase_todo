121c7382f6518981e4f34681fa248b36d1ca29fa
Hasan <thehasandev@gmail.com>
2023-11-05 19:36:52 +0600

    edit-item

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { getDatabase, ref, set,push,onValue,remove  } from "firebase/database";
+import { getDatabase, ref, set,push,onValue,remove,update   } from "firebase/database";
 import { Circles } from 'react-loader-spinner'
 function App() {
   let [name,setName] = useState("")
   let [nameList,setNameList] =useState([])
   let [loader,setLoader]=useState(false)
+  let [update,setUpdate]=useState(false)
+  let [updateIndex,setUpdateIndex] = useState("")
+ 
 
   const db = getDatabase();
 
@@ -35,12 +38,30 @@ function App() {
     remove(ref(db, 'todo/'+item.id))
    }
 
+  let handleEdit =(item)=>{
+    setName(item.userName)
+    setUpdate(true)
+    setUpdateIndex(item.id)
+  } 
+
+  let handleUpdateTodo =()=>{
+    set(ref(db, 'todo/'+updateIndex), {
+      userName : name
+      }).then(()=>{
+        setLoader(false)
+      })
+  }
+
 
   return (
     <div className='max-w-7xl mx-auto pt-10'>
       <label className='text-lg font-medium'>Name : </label>
        <input value={name} type="text" className='border-black/70 border py-2' onChange={(e)=>{setName(e.target.value)}}/>
        {
+        update ? 
+          <button onClick={handleUpdateTodo} className='px-4 py-2 text-lg text-white bg-black/70 rounded-r-lg '>Update Todo</button>
+       :
+
         loader ?
         <button>
           <Circles
@@ -55,7 +76,9 @@ function App() {
         </button>
          :
         <button onClick={handleAddTodo} className='px-4 py-2 text-lg text-white bg-black/70 rounded-r-lg '>Add Todo</button>
-       }
+       
+        }
+     
       
       <div>
          <ol type='square'>
@@ -63,7 +86,7 @@ function App() {
             nameList.map((item,index)=>(
               <div key={index}>
               <li >{item.userName}</li>
-               <button className='text-white bg-blue-700 px-4 rounded-[2px] mr-2 py-2'>Edit</button>
+               <button onClick={()=>{handleEdit(item)}} className='text-white bg-blue-700 px-4 rounded-[2px] mr-2 py-2'>Edit</button>
                <button onClick={()=>{handleDelte(item)}} className='text-white bg-red-600 px-4 py-2 rounded-[2px]'>Delete</button>
               </div>
             ))
