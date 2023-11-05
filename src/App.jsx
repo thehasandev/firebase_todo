f58c90ad043112404d9094938d2a2ce3ffe04769
Hasan <thehasandev@gmail.com>
2023-11-05 18:39:24 +0600

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { getDatabase, ref, set,push,onValue  } from "firebase/database";
+import { Circles } from 'react-loader-spinner'
 function App() {
   let [name,setName] = useState("")
   let [nameList,setNameList] =useState([])
+  let [loader,setLoader]=useState(false)
 
   const db = getDatabase();
 
   let handleAddTodo =()=>{
+    setLoader(true)
+
     set(push(ref(db, 'todo')), {
         userName : name
-    });
+    }).then(()=>{
+      setLoader(false)
+    })
+    
     setName("")
   }
 
@@ -29,7 +36,23 @@ function App() {
     <div className='max-w-7xl mx-auto pt-10'>
       <label className='text-lg font-medium'>Name : </label>
        <input value={name} type="text" className='border-black/70 border py-2' onChange={(e)=>{setName(e.target.value)}}/>
-       <button onClick={handleAddTodo} className='px-4 py-2 text-lg text-white bg-black/70 rounded-r-lg '>Add Todo</button>
+       {
+        loader ?
+        <button>
+          <Circles
+             height="20"
+             width="20"
+             color="#4fa94d"
+             ariaLabel="circles-loading"
+             wrapperStyle={{}}
+             wrapperClass="ml-5"
+             visible={true}
+           />
+        </button>
+         :
+        <button onClick={handleAddTodo} className='px-4 py-2 text-lg text-white bg-black/70 rounded-r-lg '>Add Todo</button>
+       }
+      
       <div>
          <ol type='square'>
            {
