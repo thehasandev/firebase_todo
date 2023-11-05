1d26f725d287f97a6035f1b56061c317f0963afa
Hasan <thehasandev@gmail.com>
2023-11-05 18:19:14 +0600

    firebase-white

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { getDatabase, ref, set,push } from "firebase/database";
 function App() {
+  let [name,setName] = useState("")
+  const db = getDatabase();
+
+  let handleAddTodo =()=>{
+    set(push(ref(db, 'todo')), {
+        userName : name
+    });
+    setName("")
+  }
   return (
     <div className='max-w-7xl mx-auto pt-10'>
       <label className='text-lg font-medium'>Name : </label>
-       <input type="text" className='border-black/70 border py-2'/>
-       <button className='px-4 py-2 text-lg text-white bg-black/70 rounded-r-lg '>Add Todo</button>
+       <input value={name} type="text" className='border-black/70 border py-2' onChange={(e)=>{setName(e.target.value)}}/>
+       <button onClick={handleAddTodo} className='px-4 py-2 text-lg text-white bg-black/70 rounded-r-lg '>Add Todo</button>
     </div>
   )
 }
