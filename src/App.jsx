7e516b841b69fb9659610100ba3e6260b0313e80
Hasan <thehasandev@gmail.com>
2023-11-05 18:01:27 +0600

    todo-input-des

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <h1>Tailwind setup</h1>
+    <div className='max-w-7xl mx-auto pt-10'>
+      <label className='text-lg font-medium'>Name : </label>
+       <input type="text" className='border-black/70 border py-2'/>
+       <button className='px-4 py-2 text-lg text-white bg-black/70 rounded-r-lg '>Add Todo</button>
     </div>
   )
 }
