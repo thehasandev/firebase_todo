9684e48826a5448bf713e5e14bea0a931071eec5
Hasan <thehasandev@gmail.com>
2023-11-05 18:04:39 +0600

    fire-base-install

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyD2SiFRUr5dtJzXHJxtm2VY9PunOclKU48",
+  authDomain: "todo-bf81c.firebaseapp.com",
+  projectId: "todo-bf81c",
+  storageBucket: "todo-bf81c.appspot.com",
+  messagingSenderId: "722027020674",
+  appId: "1:722027020674:web:120d0273c65f795dc54f9b"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig
