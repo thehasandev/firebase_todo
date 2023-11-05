54f9c5f67424ad3765f1c1692e8a97bf8682b078
Hasan <thehasandev@gmail.com>
2023-11-05 20:35:09 +0600

    firebase-todo-loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { getDatabase, ref, set,push,onValue,remove  } from "firebase/database";
+import { BallTriangle } from 'react-loader-spinner'
 
 function App() {
   const db = getDatabase();
   let [name,setName] = useState("")
   let [des,setDes] = useState("")
-  let [todoList,setTodoList] =useState([])
+  let [todoList,setTodoList] = useState([])
+  let [update,setUpdate] = useState(false)
+  let [loader,setLoader] = useState(false)
 
   let handleAddTodo =()=>{
+    setLoader(true)
     set(push(ref(db, 'todo')), {
       userName : name,
       userDes  : des
     }).then(()=>{
-      console.log("Done");
+      setLoader(false)
       setName("")
       setDes("")
     })
@@ -35,14 +39,30 @@ function App() {
   let handleDelete =(item)=>{
     remove(ref(db, 'todo/'+item.id))
   }
+
   return (
     <div className='m-5'>
       <label className='font-medium text-lg '>Name : </label>
       <input value={name} onChange={(e)=>{setName(e.target.value)}} className='border border-black/40 px-2 py-2' type="text" />
       <label className='font-medium text-lg '> Des : </label>
       <input value={des} onChange={(e)=>{setDes(e.target.value)}} className='border border-black/40 px-2 py-2' type="text" />
-      <button onClick={handleAddTodo} className='px-4 py-2 text-white  bg-blue-600 rounded-r-sm'>Add todo</button>
-     
+      {
+        loader ?
+          <button>
+            <BallTriangle
+             height={30}
+             width={30}
+             radius={15}
+             color="#4fa94d"
+             ariaLabel="ball-triangle-loading"
+             wrapperClass="ml-5"
+             wrapperStyle=""
+             visible={true}
+            />
+          </button>
+        :
+        <button onClick={handleAddTodo} className='px-4 py-2 text-white  bg-blue-600 rounded-r-sm'>Add todo</button>
+      }
      {
       todoList.map((item)=>(
         <div key={item.id}>
